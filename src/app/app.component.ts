53b228f5a54ef675d136ce06b3f03e15a59f9c0f
mvtram <mvenkat0897@gmail.com>
2019-12-25 16:09:13 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  projects = [
+    {
+      title: "Angular1",
+      description: 'angular1',
+      links: 'https://maheshvee23.blogspot.com/'
+    },
+    {
+      title: "Angular2",
+      description: 'angular2',
+      links: 'https://maheshvee23.blogspot.com/'
+    },
+    {
+      title: "Angular3",
+      description: 'angular3',
+      links: 'https://maheshvee23.blogspot.com/'
+    },
+
+];
 }
